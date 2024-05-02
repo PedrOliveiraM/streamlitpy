83132d64175720e0d83b96707940503151c50b32
PedrOliveiraM <pedropucmont@gmail.com>
2024-05-02 14:37:32 -0300

    deploy pdf generate

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from fpdf import FPDF
+import io
 
 st.set_page_config(page_title="App Economia Teste")
 st.title('ECONOMIA - APLICAÇÃO')
@@ -23,18 +24,25 @@ def generate_pdf(name, age):
     pdf.cell(200, 10, txt = "Nome: " + name, ln = True)
     pdf.cell(200, 10, txt = "Idade: " + str(age), ln = True)
     pdf.cell(200, 10, txt = message, ln = True)
-
-    # Salva o PDF com o nome "relatorio.pdf"
-    pdf.output("relatorio.pdf")
+    
+    # Salva o PDF em memória
+    pdf_bytes = pdf.output(dest='S').encode('latin1')
+    
+    return pdf_bytes
 
 # Entrada para configurar o host e a porta
 # Nome COMPLETO
 name = st.text_input("Nome Completo", placeholder="Digite seu nome completo")
 age = st.number_input("Idade", min_value=0, max_value=200)
 
-button = st.button("Gerar")
+# Chama a função para gerar o PDF
+pdf_bytes = generate_pdf(name, age)
 
-if button:
-    # Chama a função para gerar o PDF
-    generate_pdf(name, age)
-    st.success("PDF gerado com sucesso!")
+# Exibe um botão para baixar o PDF
+st.download_button(
+    label="Baixar PDF",
+    data=pdf_bytes,
+    file_name="relatorio.pdf",
+    mime="application/pdf"
+)
+st.success("PDF gerado com sucesso!")
