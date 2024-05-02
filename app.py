070e5297416a90374021cfa4d56457bea3761efd
PedrOliveiraM <pedropucmont@gmail.com>
2024-05-02 14:25:33 -0300

    fix: update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
+from fpdf import FPDF
 
 st.set_page_config(page_title="App Economia Teste")
 st.title('ECONOMIA - APLICAÇÃO')
@@ -13,22 +12,24 @@ def generate_pdf(name, age):
     else:
         message = f"Faltam {65 - age} anos para você se aposentar"
 
-    # Cria o conteúdo do relatório
-    nome = f"Nome: {name} "
-    idade =  f"Idade {age}"
-    result = message
-    # Cria um arquivo PDF
-    c = canvas.Canvas("relatorio.pdf", pagesize=letter)
-      # Adiciona o texto ao PDF
-    c.drawString(100, 750, f"Nome: {name}")
-    c.drawString(100, 730, f"Idade: {age}")
-    c.drawString(100, 710, message)
+    # Cria um objeto PDF
+    pdf = FPDF()
+    pdf.add_page()
+    
+    # Define a fonte e o tamanho do texto
+    pdf.set_font("Arial", size = 12)
+    
+    # Adiciona o texto ao PDF
+    pdf.cell(200, 10, txt = "Nome: " + name, ln = True)
+    pdf.cell(200, 10, txt = "Idade: " + str(age), ln = True)
+    pdf.cell(200, 10, txt = message, ln = True)
 
-    c.save()
+    # Salva o PDF com o nome "relatorio.pdf"
+    pdf.output("relatorio.pdf")
 
 # Entrada para configurar o host e a porta
-#Nome COMPLETO
-name = st.text_input("Nome Completo",placeholder="Digite seu nome completo")
+# Nome COMPLETO
+name = st.text_input("Nome Completo", placeholder="Digite seu nome completo")
 age = st.number_input("Idade", min_value=0, max_value=200)
 
 button = st.button("Gerar")
