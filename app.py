cd351b99e5892521e1aaec3576a414d09240124b
PedrOliveiraM <pedropucmont@gmail.com>
2024-05-02 14:42:54 -0300

    fix: pdf button

--- app.py
+++ app.py
@@ -35,14 +35,16 @@ def generate_pdf(name, age):
 name = st.text_input("Nome Completo", placeholder="Digite seu nome completo")
 age = st.number_input("Idade", min_value=0, max_value=200)
 
-# Chama a função para gerar o PDF
-pdf_bytes = generate_pdf(name, age)
-
-# Exibe um botão para baixar o PDF
-st.download_button(
-    label="Baixar PDF",
-    data=pdf_bytes,
-    file_name="relatorio.pdf",
-    mime="application/pdf"
-)
-st.success("PDF gerado com sucesso!")
+# Botão para gerar o PDF
+if st.button("Gerar PDF"):
+    # Chama a função para gerar o PDF
+    pdf_bytes = generate_pdf(name, age)
+    
+    # Exibe um botão para baixar o PDF
+    st.download_button(
+        label="Baixar PDF",
+        data=pdf_bytes,
+        file_name="relatorio.pdf",
+        mime="application/pdf"
+    )
+    st.success("PDF gerado com sucesso!")
